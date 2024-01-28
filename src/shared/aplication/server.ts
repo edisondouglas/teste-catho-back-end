902b81a598f5eaf3112db29a53269baa034edc73
Edison Douglas <edisondrs@gmail.com>
2024-01-28 17:01:50 -0300

    fix: fix cors

--- src/shared/aplication/server.ts
+++ src/shared/aplication/server.ts
@@ -5,16 +5,16 @@ import { AppDataSource } from "../infra/typeorm/configs/data-source";
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
 app.use(router);
 
-app.use(cors());
-
 AppDataSource.initialize()
   .then(() => {
     console.log("Data Source has been initialized!");
-    app.listen(3333, () => console.log("Server is running on port 3333"));
+    app.listen(3308, () => console.log("Server is running on port 3308"));
   })
   .catch((err) => {
     console.error("Error during Data Source initialization", err);
